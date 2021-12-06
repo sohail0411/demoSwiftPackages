a3faba29af021d9987baab0719d394245bab0878
sohail.shaik <sohail.shaik@diatoz.com>
2021-12-06 13:04:22 +0530

    Initial Commit

---
+++ Shared/ViewModifier_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModifier_DemoApp.swift
+//  Shared
+//
+//  Created by Sohail Shaik on 06/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ViewModifier_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
